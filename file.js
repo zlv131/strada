a9498e1635678a74bc6c40aac9a13e1fedf2c75a
zlv131 <skorpion156@mail.ru>
2022-10-21 12:51:34 +0300

    Сделан TODO

--- file.js
+++ file.js
@@ -1,5 +1,67 @@
-console.log('strada.one')
-console.log('strada.one')
-console.log('strada.one')
-console.log('strada.one')
-console.log('strada.one')
+// let user = {
+// 	name: 'John'
+// }
+//
+// let data = {
+// 	weight: 70,
+// 	height: 180
+// }
+//
+//
+// // for (let dataKey in data) {
+// // 	user[dataKey] = data[dataKey]
+// // }
+//
+// Object.assign(user, data)
+//
+// console.log(user)
+
+// const user = {
+// 	name: 'John',
+// 	age: 30,
+// 	sayHi() {
+// 		console.log('Hi!')
+// 	}
+// }
+//
+// user.sayHi()
+
+
+
+// let calculator = {
+// 	num1: 0,
+// 	num2: 0,
+// 	sumNums: 0,
+// 	mulNums: 0,
+// 	read(){
+// 		this.num1 = prompt('Введите первое число: ')
+// 		this.num2 = prompt('Введите второе число: ')
+// 	},
+// 	sum(){
+// 		return this.sumNums = Number(this.num1) + Number(this.num2)
+// 	},
+// 	mul(){
+// 		return this.mulNums = Number(this.num1) * Number(this.num2)
+// 	}
+// }
+// calculator.read()
+// alert(calculator.sum())
+// alert(calculator.mul())
+
+
+// let calculator = {
+// 	read(){
+// 		this.num1 = prompt('Введите первое число: ')
+// 		this.num2 = prompt('Введите второе число: ')
+// 	},
+// 	sum(){
+// 		return Number(this.num1) + Number(this.num2)
+// 	},
+// 	mul(){
+// 		return Number(this.num1) * Number(this.num2)
+// 	}
+// }
+// calculator.read()
+// alert(calculator.sum())
+// alert(calculator.mul())
+
